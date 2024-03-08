85986859a011b692970848e783343685798ed7b4
0svaldojunior <app.o.r.jr@gmail.com>
2024-03-08 18:55:16 -0300

    feat(auth): Implemented JWT authentication and token generation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,5 @@
+import { AuthModule } from './auth/auth.module'
+import { AuthenticateController } from './controllers/authenticate.controller'
 import { ConfigModule } from '@nestjs/config'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { Module } from '@nestjs/common'
@@ -10,8 +12,9 @@ import { envSchema } from './env'
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
-  controllers: [CreateAccountController],
+  controllers: [CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
